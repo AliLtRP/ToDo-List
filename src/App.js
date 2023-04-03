9785d467ceeeff205b489635320d25a82f5ceac0
AliLtRP <albaghdadyali1@gmail.com>
2023-04-03 20:46:52 +0300

    solve add items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [newItem, setNewItems] = useState('');
+  const [date, setDate] = useState('');
+  const [status, setStatus] = useState('');
+
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+
+    const item = {
+      title: newItem,
+      date: date,
+      status: status
+    };
+
+    setItems(oldList => [...oldList, item]);
+    setNewItems('');
+  }
+
+  const a = 'not started';
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={newItem} onChange={e => setNewItems(e.target.value)} />
+      <input type="date" value={date} onChange={e => setDate(e.target.value)} />
+      
+      <select onClick={e => setStatus(e.target.value)}>
+        <option>not started</option>
+        <option>complite</option>
+      </select>
+
+      <button onClick={() => addItem()}>add</button>
+
+      <ul>
+        {items.map((item)=> <li>{[item.title, item.date, item.status]}</li>)}
+      </ul>
     </div>
+
+
+
   );
 }
 
